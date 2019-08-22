288f6a0f38ee2d082887719a777759ce79310558
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-08-22 17:34:34 +0100

    rewrite code for graduate due to previous misunderstanding of what was required

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -47,13 +47,18 @@ let Gabe = new Instructor ('Gabe', 'unknown', 'Spain', 'Teaching Code', 'JS', 'T
     sprintChallenge(subject){
         console.log(`${this.name} has begun sprint challenge on ` + subject + `.`);
     }
+    
     graduate(){
-        if (this.grade >= 70){
+      
+            while (this.grade <70){
+                console.log(`Go back to grading your assignments, ${this.name}... You're not ready to graduate just yet. Let's regrade you...`);     
+                this.grade = Math.random() * (100 - 0) + 0; 
+            }
+            if(this.grade>=70){
+
             console.log(`${this.name} gets to graduate! Congratulations!`);
-        }
-        else {
-            console.log(`Go back to grading your assignments, ${this.name}... You're not ready to graduate just yet.`)
-        };
+
+            }      
     }
 };
 
